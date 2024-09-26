4d16f4466a5d52742bb1ae4cf55f7468721efb72
bywilliamlima <williamlimavg@gmail.com>
2024-09-26 17:25:04 -0300

    Sevice_API

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NonNullableFormBuilder, Validators } from '@angular/forms';
 import { Location } from '@angular/common';
 import { ClientesService } from '../services/clientes.service';
-import { MatLegacySnackBar as MatSnackBar } from '@angular/material/legacy-snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 import { Clientes } from '../model/clientes';
 
